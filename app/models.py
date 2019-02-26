a7a7438ac620ebc7ec8ad2393644cf703485d12d
PiotrKedra <piotr.k9750@gmail.com>
2019-02-26 17:53:41 +0100

    created basic model and basic response

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    restaurant_name = models.CharField(unique=True, max_length=50)
+    data_posted = models.DateTimeField(auto_now_add=True)
+    latitude = models.FloatField(default=None, blank=True, null=True)
+    longitude = models.FloatField(default=None, blank=True, null=True)
+    facebook_id = models.IntegerField(default=None, blank=True, null=True)
+    restaurant_url = models.CharField(max_length=100, default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.restaurant_name
+
+
+class Promotion(models.Model):
+    restaurant_name = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    data_posted = models.DateTimeField(auto_now_add=True)
+    data_start = models.DateField(default=None, blank=True, null=True)
+    data_end = models.DateField(default=None, blank=True, null=True)
+    promotion_message = models.TextField(default=None, blank=True, null=True)
+    promotion_name = models.CharField(max_length=100, default='Promocja')
+
+    def __str__(self):
+        return f'{self.restaurant_name}: {self.promotion_name} -> {self.promotion_message}'
+
--- app/views.py
+++ app/views.py
@@ -3,15 +3,34 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.shortcuts import render
 
-@login_required
+from app.models import Restaurant
+
+
 def users(request):
 
     if request.method == 'GET':
 
-        all_users = list(User.objects.values('id', 'username', 'email'))
 
+        longitude = request.GET.get('longitude')
+        latitude = request.GET.get('latitude')
+        radius = request.GET.get('radius')
 
-        print(request.GET)
-        return JsonResponse(all_users, safe=False)
+        # longitude = float(longitude)
+        # latitude = float(latitude)
+        # radius = float(radius)
+
+        data = []
+
+        for restaurant in Restaurant.objects.all():
+            tmp = {
+                'restaurant_name': restaurant.restaurant_name,
+                'longitude': restaurant.longitude,
+                'latitude': restaurant.latitude,
+                'restaurant_url': restaurant.restaurant_url,
+            }
+
+            data.append(tmp)
+
+        return JsonResponse(data, safe=False)
     else:
         return JsonResponse({'GET': None})
