f20e23c413497d310cc28cc18d0a654ef81f19ea
PiotrKedra <piotr.k9750@gmail.com>
2019-02-26 18:28:01 +0100

    basic response, need to add distance counter

--- app/views.py
+++ app/views.py
@@ -1,36 +1,47 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+import sys
+
 from django.http import JsonResponse
-from django.shortcuts import render
 
 from app.models import Restaurant
 
 
 def users(request):
-
     if request.method == 'GET':
-
 
         longitude = request.GET.get('longitude')
         latitude = request.GET.get('latitude')
         radius = request.GET.get('radius')
 
-        # longitude = float(longitude)
-        # latitude = float(latitude)
-        # radius = float(radius)
+        if longitude is None or latitude is None or radius is None:
+            return JsonResponse({
+                'longitude': longitude,
+                'latitude': latitude,
+                'radius': radius
+            })
 
-        data = []
+        try:
+            longitude = float(longitude)
+            latitude = float(latitude)
+            radius = float(radius)
+        except TypeError as type_error:
+            print(type_error)
+            return JsonResponse({'Parsing went wrong': str(type_error)})
+        except:
+            print(sys.exc_info()[0])
+            return JsonResponse({'Parsing went wrong': str(sys.exc_info()[0])})
 
-        for restaurant in Restaurant.objects.all():
+        # TODO need to pick up closest restaurant, not all
+        restaurants = Restaurant.objects.all()
+
+        data = {}
+        for restaurant in restaurants:
             tmp = {
-                'restaurant_name': restaurant.restaurant_name,
                 'longitude': restaurant.longitude,
                 'latitude': restaurant.latitude,
                 'restaurant_url': restaurant.restaurant_url,
             }
+            data.update({restaurant.restaurant_name: tmp})
 
-            data.append(tmp)
-
-        return JsonResponse(data, safe=False)
+        return JsonResponse(data)
     else:
         return JsonResponse({'GET': None})
