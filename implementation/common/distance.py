52f5032b74ffbd7fd997c1e272aac8fbdce46135
PiotrKedra <piotr.k9750@gmail.com>
2019-02-28 12:43:13 +0100

    distance counter

---
+++ implementation/common/distance.py
@@ -0,0 +1,19 @@
+from math import sin, cos, sqrt, atan2, radians
+
+
+def distance(lat1: float, lon1: float, lat2: float, lon2: float):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
